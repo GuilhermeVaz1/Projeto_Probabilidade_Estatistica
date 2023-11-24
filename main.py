b519dd50784a24e9bbb93bb3d13a4072962d7cd2
GuilhermeVaz1 <guisnvaz@gmail.com>
2023-11-23 23:47:25 -0300

    python dataframes + info hardware

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import os
-import datetime
+import cpuinfo
+import psutil
+import pandas as pd
+
+# informação de hardware
+cpu = cpuinfo.get_cpu_info()['brand_raw']
+mem_gb = psutil.virtual_memory().total / pow(2,30)
+
+# gerando logs de tempos de execução...
 os.system("log.exe")
-os.rename("bubble_log.csv", f"bubble_log_{datetime.datetime.now().strftime('%d_%m_%Y_%Hh_%Mm')}.csv")
-os.rename("merge_log.csv", f"merge_log_{datetime.datetime.now().strftime('%d_%m_%Y_%Hh_%Mm')}.csv")
-os.rename("quick_log.csv", f"quick_log_{datetime.datetime.now().strftime('%d_%m_%Y_%Hh_%Mm')}.csv")
+
+quick_sortDf = pd.read_csv("quick_log.csv", sep=",")
+print(quick_sortDf)
